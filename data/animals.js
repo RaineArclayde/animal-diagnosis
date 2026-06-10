799723086d9802e796096cad1c7ef16636721187
RaineArclayde <yr8g9w7fmk@privaterelay.appleid.com>
2026-06-10 11:34:15 +0900

    Create animals.js

---
+++ data/animals.js
@@ -0,0 +1,85 @@
+const animals = {
+    fox: {
+        name: "キツネ",
+        image: "images/fox.png",
+        keywords: ["好奇心", "探究心", "創造性"]
+    },
+
+    squirrel: {
+        name: "リス",
+        image: "images/squirrel.png",
+        keywords: ["計画性", "努力家", "準備上手"]
+    },
+
+    bird: {
+        name: "トリ",
+        image: "images/bird.png",
+        keywords: ["自由", "視野の広さ", "発想力"]
+    },
+
+    sheep: {
+        name: "ヒツジ",
+        image: "images/sheep.png",
+        keywords: ["協調性", "優しさ", "安心感"]
+    },
+
+    rabbit: {
+        name: "ウサギ",
+        image: "images/rabbit.png",
+        keywords: ["感受性", "直感", "繊細さ"]
+    },
+
+    badger: {
+        name: "アナグマ",
+        image: "images/badger.png",
+        keywords: ["忍耐", "探究心", "粘り強さ"]
+    },
+
+    deer: {
+        name: "シカ",
+        image: "images/deer.png",
+        keywords: ["気品", "優雅さ", "観察力"]
+    },
+
+    turtle: {
+        name: "カメ",
+        image: "images/turtle.png",
+        keywords: ["慎重", "安定", "継続力"]
+    },
+
+    goat: {
+        name: "ヤギ",
+        image: "images/goat.png",
+        keywords: ["挑戦", "独立心", "行動力"]
+    },
+
+    hedgehog: {
+        name: "ハリネズミ",
+        image: "images/hedgehog.png",
+        keywords: ["警戒心", "誠実", "防御力"]
+    },
+
+    snake: {
+        name: "ヘビ",
+        image: "images/snake.png",
+        keywords: ["知性", "戦略", "冷静さ"]
+    },
+
+    flyingSquirrel: {
+        name: "モモンガ",
+        image: "images/flying-squirrel.png",
+        keywords: ["柔軟性", "好奇心", "順応力"]
+    },
+
+    duck: {
+        name: "アヒル",
+        image: "images/duck.png",
+        keywords: ["親しみやすさ", "社交性", "温厚"]
+    },
+
+    cat: {
+        name: "ネコ",
+        image: "images/cat.png",
+        keywords: ["自由気まま", "観察力", "独創性"]
+    }
+};
