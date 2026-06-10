45d58a3dd7f4b6c0712bef11ae0c5c88f3d8dd3a
RaineArclayde <yr8g9w7fmk@privaterelay.appleid.com>
2026-06-10 11:15:35 +0900

    Update questions.js

--- data/questions.js
+++ data/questions.js
@@ -1,1 +1,16 @@
-
+const questions = [
+  {
+    question: "質問1",
+    choices: [
+      "選択肢A",
+      "選択肢B"
+    ]
+  },
+  {
+    question: "質問2",
+    choices: [
+      "選択肢A",
+      "選択肢B"
+    ]
+  }
+];
